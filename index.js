8cd6c67e36ef9aa1a54278a30c001fa53ef8db7a
twitin13 <tintagusti3@gmail.com>
2025-10-22 23:36:39 +0700

    Fix formatting and update tweet text

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ Dari Pada Blonde Creamy Latte DISKON jadi 2K-an aja bisa pick up ✨
 https://spf.shopee.co.id/BKrYkdVOG
 https://spf.shopee.co.id/BKrYkdVOG
 
-t. daget gofood grabfood kode promo go grab shopee food sfood gojek kopi
+t. daget gofood grabfood kode promo go grab shopee food raisa hamish sfood gojek kopi
 `;
 
     // 📸 Upload 1 gambar dari repo
@@ -36,4 +36,5 @@ t. daget gofood grabfood kode promo go grab shopee food sfood gojek kopi
     console.error('❌ Gagal kirim tweet:', error);
   }
 })();
+
 
